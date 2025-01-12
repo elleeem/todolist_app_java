a811ad25ed331506c36a5a043845e2f12756697d
maellea <161613995+elleeem@users.noreply.github.com>
2025-01-12 23:40:44 +0100

    Add files via upload

---
+++ src/main/java/com/example/quatreapptodolist/Adapter/ToDoAdapter.java
@@ -0,0 +1,92 @@
+package com.example.quatreapptodolist.Adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quatreapptodolist.AddNewTask;
+import com.example.quatreapptodolist.MainActivity;
+import com.example.quatreapptodolist.Model.ToDoModel;
+import com.example.quatreapptodolist.R;
+import com.example.quatreapptodolist.Utils.DataBaseHelper;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> {
+
+    private List<ToDoModel> mList;
+    private MainActivity activity;
+    private DataBaseHelper myDB;
+
+    public ToDoAdapter(DataBaseHelper myDB, MainActivity activity){
+        this.activity = activity;
+        this.myDB = myDB;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_layout, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final ToDoModel item = mList.get(position);
+        holder.checkBox.setText(item.getTask());
+        holder.checkBox.setChecked(toBoolean(item.getStatus()));
+        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (compoundButton.isChecked()){
+                    myDB.updateStatus(item.getId(),1);
+                }else {
+                    myDB.updateStatus(item.getId(),0);
+                }
+            }
+        });
+    }
+
+    public boolean toBoolean(int num){
+        return num!=0;
+    }
+
+    public Context getContext(){
+        return activity;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public void deleteTask(int position){
+        ToDoModel item = mList.get(position);
+        myDB.deleteTask(item.getId());
+
+        mList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void editItems(int position) {
+
+    }
+
+    public void setTasks(List<ToDoModel> mList) {
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        CheckBox checkBox;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            checkBox = itemView.findViewById(R.id.checkbox);
+        }
+    }
+}
